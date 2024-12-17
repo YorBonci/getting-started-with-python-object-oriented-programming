6dc6be8977452d3f50085ee8d517de84289fe70c
Yor Bonci <65611155+YorBonci@users.noreply.github.com>
2024-12-17 13:21:31 +0000

    adding second lesson's changes

--- start/02_03/circle.py
+++ start/02_03/circle.py
@@ -1,3 +1,20 @@
 """
 A class for representing a circle
 """
+
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+        self.pie = 3.14
+    
+    def calculate_circumference(self):
+        print(f"Circumference: {2 * self.pie * self.radius} units")
+
+    def calculate_area(self):
+        print(f"Area: {self.pie * (self.radius ** 2) } square units")
+
+new_circle = Circle(2)
+
+new_circle.calculate_circumference()
+new_circle.calculate_area()
